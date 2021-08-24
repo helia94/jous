5e66cbc9aba96d8daec1cf3b87c852b351e0b051
Helia Jamshidi <Helia.Jamshidi@ptvgroup.com>
2021-08-25 00:46:01 +0200

    toggle in userpage

--- frontend/src/components/UserPage.jsx
+++ frontend/src/components/UserPage.jsx
@@ -3,7 +3,7 @@ import TweetItem from "./TweetItem";
 import Axios from "axios";
 
 class UserPage extends React.Component {
-    state = { tweets: [], currentUser: { username: "" } }
+    state = { tweets: [], currentUser: { username: "" }, active: "q" }
 
     componentDidMount() {
         Axios.post("/api/userquestions", { username: this.props.match.params.username }).then(res => {
@@ -21,33 +21,45 @@ class UserPage extends React.Component {
         }, 500)
     }
 
+    toAnswers = (e) => {
+        this.setState({ active: "a" })
+    }
+
+    toQuestion = (e) => {
+        this.setState({ active: "q" })
+    }
+
     render() {
         return (
             <React.Fragment>
-                <div class="ui two item menu">
-                    <a class="active item">Questions</a>
-                    <a class="item">Answers</a>
-                </div>
-                <div
-                    className="w3-container w3-jumbo"
-                    style={{ margin: "3rem", paddingLeft: "1rem" }}>
-                    <h1>{this.props.match.params.username} questions</h1>
-                </div>
-                <div class="ui feed">
-                    {this.state.tweets.map((item, index) => {
-                        return (
-                            <div class="event">
-                                <TweetItem
-                                    id={item.id}
-                                    content={item.content}
-                                    author={item.username}
-                                    time={item.time}
-                                    isOwner={this.state.currentUser.username === item.username}
-                                    key={index}
-                                />
-                            </div>
-                        );
-                    })}
+                <div class="ui basic segment" style={{ width: 400 }}>
+                    <div className="ui basic segment">
+                        <div class="ui yellow large header">{this.props.match.params.username}</div>
+                    </div>
+                    <div class="ui tabular menu">
+                        <a class={(this.state.active == "q" ? "active" : "") + " item"}
+                            onClick={this.toQuestion}>Questions</a>
+                        <a class={(this.state.active == "a" ? "active" : "") + " item"}
+                            onClick={this.toAnswers}>Answers</a>
+                    </div>
+                    <div class="ui feed">
+                        {this.state.active == "q" ?
+                            this.state.tweets.map((item, index) => {
+                                return (
+                                    <div class="event">
+                                        <TweetItem
+                                            id={item.id}
+                                            content={item.content}
+                                            author={item.username}
+                                            time={item.time}
+                                            isOwner={this.state.currentUser.username === item.username}
+                                            key={index}
+                                        />
+                                    </div>
+                                );
+                            })
+                            : <div class="event"></div>}
+                    </div>
                 </div>
             </React.Fragment>
         );
