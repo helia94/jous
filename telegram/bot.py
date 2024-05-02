c068b60f182e7c274cc8e62524c4f71356290172
helia <helia.jm@gmail.com>
2024-05-02 15:09:43 +0200

    added logging to bot

--- telegram/bot.py
+++ telegram/bot.py
@@ -1,3 +1,4 @@
+import logging
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 import requests
@@ -9,22 +10,33 @@ TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 # Define the API endpoint URL
 API_URL = 'https://jous.app/api/question/random'
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.info("User %s started the bot.", update.message.from_user.id)
     keyboard = [[InlineKeyboardButton("Next", callback_data='next')]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text("Hello curious stranger, let's dig some juice, shall we?", reply_markup=reply_markup)
+    logger.info("Responded to your start bot.", update.message.from_user.id)
 
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.info("User %s clicked the button.", update.callback_query.from_user.id)
     query = update.callback_query
     await query.answer()
     response = requests.get(API_URL)
-    question_content = response.json()['question']['content']
-    keyboard = [[InlineKeyboardButton("Next question", callback_data='next')]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text(text=f"{question_content}", reply_markup=reply_markup)
+    if response.status_code == 200:
+        question_content = response.json()['question']['content']
+        keyboard = [[InlineKeyboardButton("Next question", callback_data='next')]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await query.edit_message_text(text=f"{question_content}", reply_markup=reply_markup)
+        logger.info("Fetched and displayed a new question.")
+    else:
+        logger.error("Failed to fetch question. Status code: %s", response.status_code)
 
 def main() -> None:
+    logger.info("Starting the bot.")
     application = Application.builder().token(TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
