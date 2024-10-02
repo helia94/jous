d0b507dec58dabca7c526b08e8078ca51a16e95b
helia <helia.jamshidi@ptvlogistics.com>
2024-10-02 12:36:21 +0200

    simplify about

--- frontend/src/components/Homev2.jsx
+++ frontend/src/components/Homev2.jsx
@@ -47,15 +47,18 @@ function Homev2() {
         textShadow: '0px 1px 2px rgba(0, 0, 0, 0.3)'
     };
 
-    const [isHeyModalOpen, setIsHeyModalOpen] = useState(false);
     const [isAboutModalOpen, setIsAboutModalOpen] = useState(false);
+    const toggleAbout = () => setIsAboutModalOpen(!isAboutModalOpen);
+    {/*
+    const [isHeyModalOpen, setIsHeyModalOpen] = useState(false);
     const [isReadModalOpen, setIsReadModalOpen] = useState(false);
     const [isWriteModalOpen, setIsWriteModalOpen] = useState(false);
 
     const toggleHey = () => setIsHeyModalOpen(!isHeyModalOpen);
-    const toggleAbout = () => setIsAboutModalOpen(!isAboutModalOpen);
     const toggleRead = () => setIsReadModalOpen(!isReadModalOpen);
     const toggleWrite = () => setIsWriteModalOpen(!isWriteModalOpen);
+    */}
+
     return (
 
         <React.Fragment>
@@ -67,7 +70,8 @@ function Homev2() {
                 <meta property="og:title" content="Jous - Deep Conversation Starters" />
                 <meta property="og:description" content="Are you easily bored with small talk? Explore and share deep, introspective questions to spark meaningful conversations. For friends, family, and dates. No login required." />
                 <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://www.jous.app/" />            </Helmet>
+                <meta property="og:url" content="https://www.jous.app/" />
+            </Helmet>
             <div style={backgroundStyle}>
                 <div className="ui center aligned inverted segment transparent-yellow">
                     <h1 className="w3-jumbo">Jous</h1>
@@ -83,30 +87,16 @@ function Homev2() {
                                 {/* Questions Modal Trigger */}
                                 <Button fluid style={buttonStyle} onClick={toggleAbout} >About Jous</Button>
                             </Grid.Column>
-                            <Grid.Column>
-                                {/* Questions Modal Trigger */}
+                            {/*<Grid.Column>
                                 <Button fluid style={buttonStyle} onClick={toggleRead}>Avoid small talk</Button>
                             </Grid.Column>
                             <Grid.Column>
-                                {/* Questions Modal Trigger */}
                                 <Button fluid style={buttonStyle} onClick={toggleWrite} >Be a creator</Button>
-                            </Grid.Column>
+                            </Grid.Column> */}
                         </Grid.Row>
                     </Grid>
                 </div>
-                {/* Hi Modal */}
-                <Modal open={isHeyModalOpen} onClose={toggleHey} closeIcon centered={false} className="animated-modal">
-                    <Modal.Header>Hey</Modal.Header>
-                    <Modal.Content scrolling>
-                        <Modal.Description>
-                            {
-                                <p>
-                                    Hi, I'm glad you found your way here. I'm Helia from Karlsruhe, and I made Jous [just pronounced like juice], mainly for myself, but also for my friends and other people trying to avoid small talk as much as possible. It is an app for collecting and sharing questions to start a conversation with.
-                                </p>
-                            }
-                        </Modal.Description>
-                    </Modal.Content>
-                </Modal>
+
 
                 {/* About Modal */}
                 <Modal open={isAboutModalOpen} onClose={toggleAbout} closeIcon centered={false} className="animated-modal">
@@ -116,75 +106,41 @@ function Homev2() {
                             {
                                 <div>
                                     <p>
-                                        Hi, I'm glad you found your way here. I'm Helia from Karlsruhe, and I made Jous [just pronounced like juice], mainly for myself, but also for my friends and other people trying to avoid small talk as much as possible. It is an app for collecting and sharing questions to start a conversation with.
+                                        Welcome to Jous, an innovative app designed for <strong>collecting and sharing intriguing questions</strong> to <strong>spark engaging conversations</strong>. This platform is not merely a forum; it is a tool to transform mundane small talk into meaningful interactions.
                                     </p>
                                     <p>
-                                        Yes, this is a website for questions, and no, it is not like Reddit. Okay, let me explain more. Let's say, that to be able to answer a question, you should either look inward, inside yourself, or outside in the world. Jous is the place only for the first type of questions. You will not find questions like <i>"what's the best budget fitness tracker?"</i> rather Like <i>"What do you often deny yourself?"</i>
+                                        <strong>Tired of the same old small talk?</strong> Jous empowers you to elevate your interactions, guiding you towards deeper, more meaningful conversations. Forget about those awkward silences and start a conversation that matters.
                                     </p>
-                                </div>
-                            }
-                        </Modal.Description>
-                    </Modal.Content>
-                </Modal>
-
-                {/* read Modal */}
-                <Modal open={isReadModalOpen} onClose={toggleRead} closeIcon centered={false} className="animated-modal">
-                    <Modal.Header>Lets help you avoid small talk</Modal.Header>
-                    <Modal.Content scrolling>
-                        <Modal.Description>
-                            {
-                                <div>
                                     <p>
-                                        So what can you do here? Next time you meet your friends and family, or you are on a date and you cannot fill the silence, you can use Jous for some nice questions. This is like one of those play cards you can buy, but I promise you, here is less cheesy. Either scroll through all questions posted by different people or use random mode to find a question. No Login is required.
+                                        Either scroll through all questions posted by different people or use <strong>random mode</strong> to find a question. No login is required.
                                     </p>
-                                    <div class="ui buttons">
-                                        <div className="ui yellow button"
-                                            onClick={() => (window.location = "/home")}>
+                                    <div className="ui buttons">
+                                        <div className="ui yellow button" onClick={() => (window.location = "/home")}>
                                             all questions
                                         </div>
-                                        <div className="ui yellow button"
-                                            onClick={() => (window.location = "/random")}>
+                                        <div className="ui yellow button" onClick={() => (window.location = "/random")}>
                                             random question
                                         </div>
                                     </div>
-                                    <br></br>
-                                    <br></br>
-                                    <div className="ui black button"
-                                        onClick={() => (window.location = "https://t.me/jous_app_bot")}>
+                                    <br /><br />
+                                    <div className="ui black button" onClick={() => (window.location = "https://t.me/jous_app_bot")}>
                                         also in telegram
                                         <i className="telegram plane icon" style={{ marginLeft: '10px' }}></i>
                                     </div>
+                                    <br /><br />
+                                    <p>
+                                        You can also be a creator of new questions. Share your own thought-provoking questions and contribute to the gathering of minds seeking meaningful connections.
+                                    </p>
+                                    <p>
+                                        Contact me for whatever reason: <a href="mailto:helia.jm@gmail.com"><strong>helia.jm@gmail.com</strong></a>
+                                    </p>
                                 </div>
                             }
                         </Modal.Description>
                     </Modal.Content>
                 </Modal>
 
-                {/* write Modal */}
-                <Modal open={isWriteModalOpen} onClose={toggleWrite} closeIcon centered={false} className="animated-modal">
-                    <Modal.Header>I cant wait to read your questions</Modal.Header>
-                    <Modal.Content scrolling>
-                        <Modal.Description>
-                            {
-                                <div>
-                                    <p>
-                                        If you want to add your own questions and contribute to the list so everybody can use them, write them down in your name or in an anonymous mode. You can also answer questions from yourself or others. Everyone can contribute in anonymous mode without login, but if you want to see your questions later under your name, you need to register, any ID will do, no emails are required.
-                                    </p>
-                                    <div class="ui buttons">
-                                        <div className="ui grey button"
-                                            onClick={() => (window.location = "/login")}>
-                                            Login
-                                        </div>
-                                        <div className="ui grey button"
-                                            onClick={() => (window.location = "/register")}>
-                                            Register
-                                        </div>
-                                    </div>
-                                </div>
-                            }
-                        </Modal.Description>
-                    </Modal.Content>
-                </Modal>
+
             </div>
         </React.Fragment>
     );
