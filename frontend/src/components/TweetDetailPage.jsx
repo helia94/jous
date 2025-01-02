b383085c257959deea942ddfed8bc44a1d799169
helia <helia.jm@gmail.com>
2025-01-02 20:59:13 +0100

    no refresh when answering

--- frontend/src/components/TweetDetailPage.jsx
+++ frontend/src/components/TweetDetailPage.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import Axios from "axios";
-import moment from 'moment'
+import moment from 'moment';
 import TweetItem2 from "./TweetItem2";
 import { Helmet } from 'react-helmet';
 
 class TweetDetailPage extends React.Component {
-    state = {
-        question: { id: 0, content: "", author: "", time: "" },
-        answers: [],
-        isLoggedIn: false,
-        currentUser: { username: "" },
-        newAnswer: "",
-        anon: "False",
-        width: 500,
+    constructor(props) {
+        super(props);
+        this.state = {
+            question: { id: 0, content: "", author: "", time: "" },
+            answers: [],
+            isLoggedIn: false,
+            currentUser: { username: "" },
+            newAnswer: "",
+            anon: "False",
+            width: 500,
+            height: 500
+        };
     }
 
     componentDidMount() {
@@ -28,12 +32,13 @@ class TweetDetailPage extends React.Component {
                         Authorization: `Bearer ${token}`
                     }
                 }).then(res => {
-                    this.setState({ isLoggedIn: true })
-                    this.setState({ currentUser: res.data })
-                })
+                    this.setState({ isLoggedIn: true, currentUser: res.data });
+                });
             }
-        }, 20)
+        }, 20);
+
         this.setState({ width: window.innerWidth, height: window.innerHeight });
+        window.addEventListener('resize', this.updateDimensions);
     }
 
     handleFormSubmit = (e) => {
@@ -41,32 +46,39 @@ class TweetDetailPage extends React.Component {
         const config = {
             headers: {}
         };
-
         if (this.state.isLoggedIn) {
             config.headers['Authorization'] = "Bearer " + localStorage.getItem("token");
         }
+
         Axios.post("/api/addanswer", {
             content: this.state.newAnswer,
             anon: this.state.anon,
             question: this.props.match.params.question
         }, config).then(res => {
             if (res.data.success) {
-                window.location.reload()
-                console.log("added answer")
+                // Remove window.location.reload() and update state directly
+                // Double check if your API returns the newly created answer in res.data.answer
+                // or build it from the current form data
+                const newAnswerObj = {
+                    content: this.state.newAnswer,
+                    username: this.state.currentUser.username,
+                    time: moment().format('ddd, DD MMM YYYY h:mm:ss') 
+                };
+                this.setState(prevState => ({
+                    answers: [...prevState.answers, newAnswerObj],
+                    newAnswer: ""
+                }));
+                console.log("added answer");
             } else {
-                console.log(res.data.error)
-                this.setState(
-                    { formErr: res.data.error }
-                )
+                console.log(res.data.error);
+                this.setState({ formErr: res.data.error });
             }
-        })
+        });
     }
 
     handleInputChange = (e) => {
         e.preventDefault();
-        this.setState({
-            newAnswer: e.target.value
-        });
+        this.setState({ newAnswer: e.target.value });
     }
 
     updateDimensions = () => {
@@ -82,9 +94,12 @@ class TweetDetailPage extends React.Component {
             <React.Fragment>
                 <Helmet>
                     <title>{`Question ${this.state.question.id}`}</title>
-                    <link rel="canonical" href={`https://jous.app/question/${this.state.question.id}`} />
+                    <link
+                        rel="canonical"
+                        href={`https://jous.app/question/${this.state.question.id}`}
+                    />
                 </Helmet>
-                <div class="ui basic segment" style={{ width: Math.min(this.state.width * 0.9, 500) }}>
+                <div className="ui basic segment" style={{ width: Math.min(this.state.width * 0.9, 500) }}>
                     <TweetItem2
                         id={this.state.question.id}
                         content={this.state.question.content}
@@ -95,19 +110,21 @@ class TweetDetailPage extends React.Component {
                         isOwner={this.state.currentUser.username === this.state.question.username}
                         key={0}
                     />
-                    <div class="ui comments">
-                        <h3 class="ui dividing header">Answers</h3>
+                    <div className="ui comments">
+                        <h3 className="ui dividing header">Answers</h3>
                         {this.state.answers.map((item, index) => {
                             return (
-                                <div class="comment">
-                                    <div class="content">
-                                        <a class="author" href={"/user/" + item.username}>{item.username}</a>
-                                        <div class="metadata">
-                                            <span class="date">{
-                                                moment(item.time, 'ddd, DD MMM YYYY h:mm:ss').format('DD MMM')
-                                            }</span>
+                                <div className="comment" key={index}>
+                                    <div className="content">
+                                        <a className="author" href={"/user/" + item.username}>
+                                            {item.username}
+                                        </a>
+                                        <div className="metadata">
+                                            <span className="date">
+                                                {moment(item.time, 'ddd, DD MMM YYYY h:mm:ss').format('DD MMM')}
+                                            </span>
                                         </div>
-                                        <div class="text">
+                                        <div className="text">
                                             {item.content}
                                         </div>
                                     </div>
@@ -115,26 +132,28 @@ class TweetDetailPage extends React.Component {
                             );
                         })}
                     </div>
-                    <form class="ui reply form" onSubmit={this.handleFormSubmit} id="submit-form">
-                        <div class="field" value={this.state.newAnswer} onChange={this.handleInputChange}>
-                            <textarea></textarea>
+                    <form className="ui reply form" onSubmit={this.handleFormSubmit} id="submit-form">
+                        <div className="field" value={this.state.newAnswer} onChange={this.handleInputChange}>
+                            <textarea value={this.state.newAnswer} />
                         </div>
-                        <div class="ui buttons">
-
-                            {this.state.isLoggedIn &&
-                                <button type="submit"
-                                    class="ui black submit icon button"
-                                    value="post"
+                        <div className="ui buttons">
+                            {this.state.isLoggedIn && (
+                                <button
+                                    type="submit"
+                                    className="ui black submit icon button"
                                     data-tooltip="add answer"
-                                    onClick={() => (this.setState({ anon: 'False' }))}>
-                                    <i class="icon edit"></i>
+                                    onClick={() => this.setState({ anon: 'False' })}
+                                >
+                                    <i className="icon edit"></i>
                                 </button>
-                            }
-                            <button type="submit"
-                                class="ui black  submit icon button"
+                            )}
+                            <button
+                                type="submit"
+                                className="ui black submit icon button"
                                 data-tooltip="add answer anonymously"
-                                onClick={() => (this.setState({ anon: 'True' }))}>
-                                <i class="user secret icon"></i>
+                                onClick={() => this.setState({ anon: 'True' })}
+                            >
+                                <i className="user secret icon"></i>
                             </button>
                         </div>
                     </form>
