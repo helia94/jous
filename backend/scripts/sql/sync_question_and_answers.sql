8b7d9ce50663f9d9b7edeed961796fe6fc35da47
helia <helia.jm@gmail.com>
2025-01-07 11:36:58 +0100

    remove useless stuff

--- backend/scripts/sql/sync_question_and_answers.sql
+++
@@ -1,6 +0,0 @@
-UPDATE question q
-SET    public_answer = t2.public_answer_updated
-FROM   (select q.id, coalesce(public_answer_updated,'{}') as public_answer_updated from question q left join 
-(select question, array_agg(publicanswer.id) as public_answer_updated from publicanswer group by publicanswer.question) as a 
-on q.id = a.question) as t2
-WHERE  t2.id = q.id;
