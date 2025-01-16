7698055c57fb47c5d052eaf4f088d9099bcd8458
helia <helia.jm@gmail.com>
2025-01-17 00:48:37 +0100

    add languages to constructor

--- frontend/src/components/Random.jsx
+++ frontend/src/components/Random.jsx
@@ -1,81 +1,85 @@
+// Random.jsx
 import React from "react";
 import Axios from "axios";
 import TweetItem2 from "./TweetItem2";
 import { Helmet } from 'react-helmet';
 import { LanguageContext } from "./LanguageContext"; 
 
-
 class Random extends React.Component {
-    state = { question: "",
-        selectedLanguageFrontendCode: "original",
-        selectedLanguageBackendCode: null
-    }
-    static contextType = LanguageContext;
+  static contextType = LanguageContext;
 
-    nextRandomQuestion = () => {
-        Axios.get("/api/question/random", { params: 
-            {
-            language_id: this.state.selectedLanguageBackendCode
-         }}).then(res => {
-            this.setState({
-                question: res.data.question,
-            })
-        });
-    }
+  constructor(props, context) {
+    super(props, context);
+    const { availableLanguages = [], language } = context;
+    const selectedLanguage = availableLanguages.find(
+      (lang) => lang.frontend_code === language
+    ) || { frontend_code: "original", backend_code: null };
 
-    componentDidMount() {
-        const { availableLanguages = [], language } = this.context; // Provide a default value for availableLanguages
-        const selectedLanguage = availableLanguages.find(
-          (lang) => lang.frontend_code === language
-        );
+    this.state = {
+      question: "",
+      selectedLanguageFrontendCode: selectedLanguage.frontend_code,
+      selectedLanguageBackendCode: selectedLanguage.backend_code
+    };
+  }
 
-        this.setState({
-            selectedLanguageFrontendCode: selectedLanguage.frontend_code,
-            selectedLanguageBackendCode: selectedLanguage.backend_code
-        })
+  componentDidMount() {
+    Axios.get("/api/question/random", {
+      params: {
+        language_id: this.state.selectedLanguageBackendCode
+      }
+    }).then(res => {
+      this.setState({
+        question: res.data.question
+      });
+    });
+  }
 
-        Axios.get("/api/question/random", { params: 
-            {
-            language_id: selectedLanguage.backend_code
-         }}).then(res => {
-            this.setState({
-                question: res.data.question,
-            })
-        });
-    }
+  nextRandomQuestion = () => {
+    Axios.get("/api/question/random", {
+      params: {
+        language_id: this.state.selectedLanguageBackendCode
+      }
+    }).then(res => {
+      this.setState({
+        question: res.data.question
+      });
+    });
+  }
 
-    render() {
-        return (
-            <React.Fragment>
-                <Helmet>
-                    <title>Random</title>
-                    <link rel="canonical" href="https://jous.app/random" />
-                </Helmet>
-                <div className="ui center aligned yellow inverted segment">
-                    <h1 className="w3-jumbo">Random Jous</h1>
-                </div>
-                <div class="ui container">
-                    <div class="event">
-                        <TweetItem2
-                            id={this.state.question.id}
-                            content={this.state.question.content}
-                            author={this.state.question.username}
-                            time={this.state.question.time}
-                            likes={this.state.question.like_number}
-                            answers={this.state.question.answer_number}
-                            isOwner={false}
-                            key={0}
-                            selectedLanguageFrontendCode={this.state.selectedLanguageFrontendCode}
-                            />
-                    </div>
-                    <div className="ui yellow button"
-                        onClick={this.nextRandomQuestion}>
-                        Next
-                    </div>
-                </div>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Helmet>
+          <title>Random</title>
+          <link rel="canonical" href="https://jous.app/random" />
+        </Helmet>
+        <div className="ui center aligned yellow inverted segment">
+          <h1 className="w3-jumbo">Random Jous</h1>
+        </div>
+        <div className="ui container">
+          <div className="event">
+            <TweetItem2
+              id={this.state.question.id}
+              content={this.state.question.content}
+              author={this.state.question.username}
+              time={this.state.question.time}
+              likes={this.state.question.like_number}
+              answers={this.state.question.answer_number}
+              isOwner={false}
+              key={0}
+              selectedLanguageFrontendCode={this.state.selectedLanguageFrontendCode}
+            />
+          </div>
+          <div
+            className="ui yellow button"
+            onClick={this.nextRandomQuestion}
+          >
+            Next
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Random;
--- frontend/src/components/TweetDetailPage.jsx
+++ frontend/src/components/TweetDetailPage.jsx
@@ -1,3 +1,4 @@
+// TweetDetailPage.jsx
 import React from "react";
 import Axios from "axios";
 import moment from 'moment';
@@ -6,187 +7,182 @@ import { Helmet } from 'react-helmet';
 import { LanguageContext } from "./LanguageContext"; 
 
 class TweetDetailPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            question: { id: 0, content: "", author: "", time: "" },
-            answers: [],
-            isLoggedIn: false,
-            currentUser: { username: "" },
-            newAnswer: "",
-            anon: "False",
-            width: 500,
-            height: 500,
-            selectedLanguageFrontendCode: "original"
-        };
-    }
+  static contextType = LanguageContext;
 
-    static contextType = LanguageContext;
-    
-    componentDidMount() {
-        const { availableLanguages = [], language } = this.context; 
-        const selectedLanguage = availableLanguages.find(
-          (lang) => lang.frontend_code === language
-        );
+  constructor(props, context) {
+    super(props, context);
+    const { availableLanguages = [], language } = context;
+    const selectedLanguage = availableLanguages.find(
+      (lang) => lang.frontend_code === language
+    ) || { frontend_code: "original", backend_code: null };
 
-        this.setState({ 
-            selectedLanguageFrontendCode: selectedLanguage.frontend_code
-        });
-
-        Axios.get("/api/question/" + this.props.match.params.question, {
-            params: { 
-                language_id: selectedLanguage.backend_code
-             } }).then(res => {
-            this.setState({ 
-                question: res.data.question, answers: res.data.answers});
-        });
-        
-        setTimeout(() => {
-            const token = localStorage.getItem("token");
-            if (token) {
-                Axios.get("/api/getcurrentuser", {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                }).then(res => {
-                    this.setState({ isLoggedIn: true, currentUser: res.data });
-                });
-            }
-        }, 20);
-        this.setState({ width: window.innerWidth, height: window.innerHeight });
-        window.addEventListener('resize', this.updateDimensions);
-    }
-
-    handleFormSubmit = (e) => {
-        e.preventDefault();
-        const config = {
-            headers: {}
-        };
-        if (this.state.isLoggedIn) {
-            config.headers['Authorization'] = "Bearer " + localStorage.getItem("token");
-        }
-
-        Axios.post("/api/addanswer", {
-            content: this.state.newAnswer,
-            anon: this.state.anon,
-            question: this.props.match.params.question
-        }, config).then(res => {
-            if (res.data.success) {
-                // Remove window.location.reload() and update state directly
-                // Double check if your API returns the newly created answer in res.data.answer
-                // or build it from the current form data
-                const newAnswerObj = {
-                    content: this.state.newAnswer,
-                    username: this.state.currentUser.username,
-                    time: moment().format('ddd, DD MMM YYYY h:mm:ss') 
-                };
-                this.setState(prevState => ({
-                    answers: [...prevState.answers, newAnswerObj],
-                    newAnswer: ""
-                }));
-                console.log("added answer");
-            } else {
-                console.log(res.data.error);
-                this.setState({ formErr: res.data.error });
-            }
-        });
-    }
-
-    handleInputChange = (e) => {
-        e.preventDefault();
-        this.setState({ newAnswer: e.target.value });
-    }
-
-    updateDimensions = () => {
-        this.setState({ width: window.innerWidth, height: window.innerHeight });
+    this.state = {
+      question: { id: 0, content: "", author: "", time: "" },
+      answers: [],
+      isLoggedIn: false,
+      currentUser: { username: "" },
+      newAnswer: "",
+      anon: "False",
+      width: 500,
+      height: 500,
+      selectedLanguageFrontendCode: selectedLanguage.frontend_code,
+      selectedLanguageBackendCode: selectedLanguage.backend_code
     };
+  }
 
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.updateDimensions);
+  componentDidMount() {
+    Axios.get("/api/question/" + this.props.match.params.question, {
+      params: {
+        language_id: this.state.selectedLanguageBackendCode
+      }
+    }).then(res => {
+      this.setState({
+        question: res.data.question,
+        answers: res.data.answers
+      });
+    });
+
+    setTimeout(() => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        Axios.get("/api/getcurrentuser", {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }).then(res => {
+          this.setState({ isLoggedIn: true, currentUser: res.data });
+        });
+      }
+    }, 20);
+
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+    window.addEventListener('resize', this.updateDimensions);
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    const config = {
+      headers: {}
+    };
+    if (this.state.isLoggedIn) {
+      config.headers['Authorization'] = "Bearer " + localStorage.getItem("token");
     }
 
-    renderTweetItem(item) {
-        return (
-            item && (
-                    <TweetItem2
-                        id={item.id}
-                        content={item.content}
-                        author={item.username}
-                        time={item.time}
-                        likes={item.like_number}
-                        answers={item.answer_number}
-                        isOwner={this.state.currentUser.username === item.username}
-                        isLoggedIn={this.state.login}
-                        selectedLanguageFrontendCode={this.state.selectedLanguageFrontendCode}
-                    />
-            )
-        );
-    }
-    
+    Axios.post("/api/addanswer", {
+      content: this.state.newAnswer,
+      anon: this.state.anon,
+      question: this.props.match.params.question
+    }, config).then(res => {
+      if (res.data.success) {
+        const newAnswerObj = {
+          content: this.state.newAnswer,
+          username: this.state.currentUser.username,
+          time: moment().format('ddd, DD MMM YYYY h:mm:ss')
+        };
+        this.setState(prevState => ({
+          answers: [...prevState.answers, newAnswerObj],
+          newAnswer: ""
+        }));
+      } else {
+        this.setState({ formErr: res.data.error });
+      }
+    });
+  }
 
-    render() {
-        return (
-            <React.Fragment>
-                <Helmet>
-                    <title>{`Question ${this.state.question.id}`}</title>
-                    <link
-                        rel="canonical"
-                        href={`https://jous.app/question/${this.state.question.id}`}
-                    />
-                </Helmet>
-                <div className="ui basic segment" style={{ width: Math.min(this.state.width * 0.9, 500) }}>
-                {this.renderTweetItem(this.state.question)}
-                    <div className="ui comments">
-                        <h3 className="ui dividing header">Answers</h3>
-                        {this.state.answers.map((item, index) => {
-                            return (
-                                <div className="comment" key={index}>
-                                    <div className="content">
-                                        <a className="author" href={"/user/" + item.username}>
-                                            {item.username}
-                                        </a>
-                                        <div className="metadata">
-                                            <span className="date">
-                                                {moment(item.time, 'ddd, DD MMM YYYY h:mm:ss').format('DD MMM')}
-                                            </span>
-                                        </div>
-                                        <div className="text">
-                                            {item.content}
-                                        </div>
-                                    </div>
-                                </div>
-                            );
-                        })}
+  handleInputChange = (e) => {
+    e.preventDefault();
+    this.setState({ newAnswer: e.target.value });
+  }
+
+  updateDimensions = () => {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+
+  renderTweetItem(item) {
+    return (
+      item && (
+        <TweetItem2
+          id={item.id}
+          content={item.content}
+          author={item.username}
+          time={item.time}
+          likes={item.like_number}
+          answers={item.answer_number}
+          isOwner={this.state.currentUser.username === item.username}
+          isLoggedIn={this.state.login}
+          selectedLanguageFrontendCode={this.state.selectedLanguageFrontendCode}
+        />
+      )
+    );
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Helmet>
+          <title>{`Question ${this.state.question.id}`}</title>
+          <link
+            rel="canonical"
+            href={`https://jous.app/question/${this.state.question.id}`}
+          />
+        </Helmet>
+        <div className="ui basic segment" style={{ width: Math.min(this.state.width * 0.9, 500) }}>
+          {this.renderTweetItem(this.state.question)}
+          <div className="ui comments">
+            <h3 className="ui dividing header">Answers</h3>
+            {this.state.answers.map((item, index) => {
+              return (
+                <div className="comment" key={index}>
+                  <div className="content">
+                    <a className="author" href={"/user/" + item.username}>
+                      {item.username}
+                    </a>
+                    <div className="metadata">
+                      <span className="date">
+                        {moment(item.time, 'ddd, DD MMM YYYY h:mm:ss').format('DD MMM')}
+                      </span>
                     </div>
-                    <form className="ui reply form" onSubmit={this.handleFormSubmit} id="submit-form">
-                        <div className="field" value={this.state.newAnswer} onChange={this.handleInputChange}>
-                            <textarea value={this.state.newAnswer} />
-                        </div>
-                        <div className="ui buttons">
-                            {this.state.isLoggedIn && (
-                                <button
-                                    type="submit"
-                                    className="ui black submit icon button"
-                                    data-tooltip="add answer"
-                                    onClick={() => this.setState({ anon: 'False' })}
-                                >
-                                    <i className="icon edit"></i>
-                                </button>
-                            )}
-                            <button
-                                type="submit"
-                                className="ui black submit icon button"
-                                data-tooltip="add answer anonymously"
-                                onClick={() => this.setState({ anon: 'True' })}
-                            >
-                                <i className="user secret icon"></i>
-                            </button>
-                        </div>
-                    </form>
+                    <div className="text">
+                      {item.content}
+                    </div>
+                  </div>
                 </div>
-            </React.Fragment>
-        );
-    }
+              );
+            })}
+          </div>
+          <form className="ui reply form" onSubmit={this.handleFormSubmit} id="submit-form">
+            <div className="field" value={this.state.newAnswer} onChange={this.handleInputChange}>
+              <textarea value={this.state.newAnswer} />
+            </div>
+            <div className="ui buttons">
+              {this.state.isLoggedIn && (
+                <button
+                  type="submit"
+                  className="ui black submit icon button"
+                  data-tooltip="add answer"
+                  onClick={() => this.setState({ anon: 'False' })}
+                >
+                  <i className="icon edit"></i>
+                </button>
+              )}
+              <button
+                type="submit"
+                className="ui black submit icon button"
+                data-tooltip="add answer anonymously"
+                onClick={() => this.setState({ anon: 'True' })}
+              >
+                <i className="user secret icon"></i>
+              </button>
+            </div>
+          </form>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default TweetDetailPage;
