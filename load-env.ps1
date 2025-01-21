087b392e256706fc2789357d538b3f99ffb1ce41
helia <helia.jm@gmail.com>
2025-01-21 17:01:57 +0100

    fix load env script

--- load-env.ps1
+++ load-env.ps1
@@ -1,4 +1,18 @@
 Get-Content .env.local | ForEach-Object {
-    $line = $_ -split '='
-    [System.Environment]::SetEnvironmentVariable($line[0], $line[1], 'Process')
+  $_.Trim() | ForEach-Object {
+    if ($_ -match '^(?<key>[^=]+)=(?<value>.*)$') {
+      $key = $Matches['key'].Trim()
+      $value = $Matches['value'].Trim().Trim('"')
+      # Force remove Windows carriage return if any.
+      $value = $value -replace "`r",""
+
+      if ($value -match '^\$\{(.+?)\}$') {
+        $envVarName = $Matches[1]
+        $envVarValue = [System.Environment]::GetEnvironmentVariable($envVarName, 'Process')
+        [System.Environment]::SetEnvironmentVariable($key, $envVarValue, 'Process')
+      } else {
+        [System.Environment]::SetEnvironmentVariable($key, $value, 'Process')
+      }
+    }
+  }
 }
