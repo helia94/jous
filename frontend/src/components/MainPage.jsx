7f479532716b304a8d9b9eb6e727f5f9354403cb
helia <helia.jm@gmail.com>
2025-01-24 20:29:19 +0100

    fix indexing

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -106,22 +106,22 @@ class MainPage extends React.Component {
         <title>Jous Home</title>
         <link rel="canonical" href="https://jous.app/home" />
         <script type="application/ld+json">
-          {JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "QAPage",
-            "mainEntity": [
-              ...this.state.tweets.map((tweet) => ({
-                "@type": "Question",
-                "name": tweet.content.substring(0, 100),  // Trim content for readability
-                "text": tweet.content,
-                "dateCreated": tweet.time,
-                "author": {
-                  "@type": "Person",
-                  "name": tweet.username
-                },
-                "answerCount": tweet.answer_number,
-                "upvoteCount": tweet.like_number,
-                "acceptedAnswer": tweet.answer_number > 0 ? {
+        {JSON.stringify({
+          "@context": "https://schema.org",
+          "@type": "FAQPage",
+          "mainEntity": this.state.tweets.map((tweet) => ({
+            "@type": "Question",
+            "name": "q",
+            "text": tweet.content,
+            "dateCreated": tweet.time,
+            "author": {
+              "@type": "Person",
+              "name": tweet.username
+            },
+            "answerCount": tweet.answer_number,
+            "upvoteCount": tweet.like_number,
+            "acceptedAnswer": tweet.answer_number > 0
+              ? {
                   "@type": "Answer",
                   "text": "This question has answers, view them on the site.",
                   "dateCreated": tweet.time,
@@ -129,10 +129,10 @@ class MainPage extends React.Component {
                     "@type": "Person",
                     "name": "Various Users"
                   }
-                } : undefined
-              }))
-            ]
-          })}
+                }
+              : undefined
+          }))
+        })}
         </script>
       </Helmet> 
         <div className="ui basic segment" style={{ width: Math.min(this.state.width, 768) }}>
